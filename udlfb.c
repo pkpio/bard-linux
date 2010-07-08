pkpio/bard-linux

commit e67ccc746f4b5f4bce878290fa5ecdfd82df6bc0

dlfb_free_urb_list is now the responsibility of dlfb_free

udlfb.c
@@ -858,7 +858,8 @@ static void dlfb_free(struct kref *kref)
 	struct dlfb_data *dev = container_of(kref, struct dlfb_data, kref);
 
 	/* this function will wait for all in-flight urbs to complete */
-	dlfb_free_urb_list(dev);
+	if (dev->urbs.count > 0)
+		dlfb_free_urb_list(dev);
 
 	if (dev->backing_buffer)
 		vfree(dev->backing_buffer);
@@ -1585,9 +1586,6 @@ error:
 
 		if (dev->backing_buffer)
 			vfree(dev->backing_buffer);
-
-		if (dev->urbs.count > 0)
-			dlfb_free_urb_list(dev);
 
 		kref_put(&dev->kref, dlfb_free); /* ref for framebuffer */
 		kref_put(&dev->kref, dlfb_free); /* last ref from kref_init */

commit 1dd39a65001deb5a84088dfabb788d3274fbb6b6

Fix shutdown hang introduced with commit e67ccc746f

udlfb.c
@@ -1814,12 +1814,12 @@ static void dlfb_free_urb_list(struct dlfb_data *dev)
 
 	/* keep waiting and freeing, until we've got 'em all */
 	while (count--) {
-		/* Timeout means a memory leak and/or fault */
-		ret = down_timeout(&dev->urbs.limit_sem, FREE_URB_TIMEOUT);
-		if (ret) {
-			BUG_ON(ret);
+
+		/* Getting interrupted means a leak, but ok at shutdown*/
+		ret = down_interruptible(&dev->urbs.limit_sem);
+		if (ret)
 			break;
-		}
+
 		spin_lock_irqsave(&dev->urbs.lock, flags);
 
 		node = dev->urbs.list.next; /* have reserved one with sem */