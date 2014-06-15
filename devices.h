d5429f076428a93f675178721a29400c2dc8b443
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-06-14 23:08:51 -0700

    Created devices header file

---
+++ devices.h
@@ -0,0 +1,12 @@
+#ifndef _DEVICES_H_
+#define _DEVICES_H_
+
+/* Supported vendors */
+#define USB_GOO_VENDOR_ID 0x18d1 /* Google */
+#define USB_SAM_VENDOR_ID 0x04e8 /* Samsung */
+
+/* Supported products */
+#define USB_GS2_PRODUCT_ID 0x6860 /* Galaxy S2 */
+#define USB_N10_PRODUCT_ID 0x4ee1 /* Nexus 10 */
+
+#endif
