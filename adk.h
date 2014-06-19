f8680d7f8e239b4bc6d68abbecbb5d7165935670
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-06-19 22:07:53 +0530

    Moved contents of aoa.h to adk.h
    Too many files to bother otherwise.

--- adk.h
+++ adk.h
@@ -1,8 +1,6 @@
 #include <linux/usb.h>
 #include <linux/slab.h>
 
-#include "aoa.h"
-
 /* ADK identification params */
 #define ADK_MAN "BeagleBone"			/* Board manufacturer */
 #define ADK_MOD "BeagleBone Black"		/* Board model */
@@ -10,6 +8,32 @@
 #define ADK_VER "1.0"				/* Board version */
 #define ADK_URI "http://beagleboard.org/\0"	/* Board url */
 #define ADK_SER "42"				/* Board serial */
+
+/* AOA protocol ids */
+#define AOA_GET_PROTOCOL		51
+#define AOA_SEND_IDENT			52
+#define AOA_START_ACCESSORY		53
+#define AOA_REGISTER_HID		54
+#define AOA_UNREGISTER_HID		55
+#define AOA_SET_HID_REPORT_DESC		56
+#define AOA_SEND_HID_EVENT		57
+#define AOA_AUDIO_SUPPORT		58
+
+/* AOA String IDs */
+#define AOA_STRING_MAN_ID		0
+#define AOA_STRING_MOD_ID		1
+#define AOA_STRING_DSC_ID		2
+#define AOA_STRING_VER_ID		3
+#define AOA_STRING_URL_ID		4
+#define AOA_STRING_SER_ID		5
+
+/* AOA Product IDs */
+#define AOA_ACCESSORY_PID		0x2d00	/* accessory */
+#define AOA_ACCESSORY_ADB_PID		0x2d01	/* accessory + adb */
+#define AOA_AUDIO_PID			0x2D02	/* audio */
+#define AOA_AUDIO_ADB_PID		0x2D03	/* audio + adb */
+#define AOA_ACCESSORY_AUDIO_PID		0x2D04	/* accessory + audio */
+#define AOA_ACCESSORY_AUDIO_ADB_PID	0x2D05	/* accessory + audio + adb */
 
 /* Structure to hold all of our device specific stuff */
 struct adk_device {
