71ddf4ad4a9b001bf496ab7f0964c47b8b9a87b8
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-06-19 13:24:23 +0530

    Added Nexus 10 debug mode pid

--- devices.h
+++ devices.h
@@ -9,4 +9,7 @@
 #define USB_GS2_PRODUCT_ID 0x6860 /* Galaxy S2 */
 #define USB_N10_PRODUCT_ID 0x4ee1 /* Nexus 10 */
 
+/* USB Debugging enabled ids */
+#define USB_N10_DEB_PRODUCT_ID 0x4ee2 /* Nexus 10 */
+
 #endif
