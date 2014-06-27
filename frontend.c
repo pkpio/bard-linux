539ef96915786351a818ebeaf1223710e7677a07
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-06-27 18:46:43 +0530

    Buffer size from parameter in uspace

--- frontend.c
+++ frontend.c
@@ -5,18 +5,19 @@
 #include <unistd.h>
 
 #define DEFAULT_DEVICE	"/dev/bard0"
+#define BUFFER_SIZE 200
 
 void send_cmd(int fd, int cmd)
 {
 	int retval = 0;
 	int i = 0;
-	char buffer[6];
+	char buffer[BUFFER_SIZE];
 	
-	for(i=0;i<6;i++){
+	for(i=0;i<BUFFER_SIZE;i++){
 		buffer[i] = 0x6f;
 	}
 	
-	retval = write(fd, buffer, 6);
+	retval = write(fd, buffer, BUFFER_SIZE);
 	if (retval < 0)
 		fprintf(stderr, "an error occured: %d\n", retval);
 }
