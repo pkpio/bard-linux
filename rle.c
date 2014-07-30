5cdc38129423092c861c54ce5b902031473c456b
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-07-30 07:39:09 +0530

    RLE with special char support complete

--- rle.c
+++ rle.c
@@ -22,19 +22,56 @@ char *compress(char *str) {
 		++run_len;
 		printf("run_len: %d\n", run_len);
 		
+		// end of run
 		if (!(*c_last2) || run_len == 255 || *c_last1 != *c_first1 
 			|| *c_last2 != *c_first2) { 
-			// end of run
-			*c_write1 = *c_first1;
-			*c_write2 = *c_first2;
 			
+			// No repition. Input as output.
+			if(run_len < 2){
+				*c_write1 = *c_first1;
+				*c_write2 = *c_first2;
+			}
+			
+			// repeated twice. Input as output.
+			if(run_len == 2){
+				// Write the input
+				*c_write1 = *c_first1;
+				*c_write2 = *c_first2;
+				
+				c_write1 = c_write1 + 2;
+				c_write2 = c_write2 + 2;
+				
+				// Write the input again. No encoding.
+				*c_write1 = *c_first1;
+				*c_write2 = *c_first2;				
+			}
+			
+			// run_len >= 3. We also add a special char.
+			else if(run_len > 2){
+				// Skip 2 bytes. We shall write the special char
+				// here at the end.
+				c_write1 = c_write1 + 2;
+				c_write2 = c_write2 + 2;
+				
+				// Write run_len
+				*c_write1 = '0' + run_len;
+				
+				c_write1 = c_write1 + 1;
+				c_write2 = c_write2 + 1;
+				
+				// Write the repeated bytes
+				*c_write1 = *c_first1;
+				*c_write2 = *c_first2;
+				
+				// Now go back and write the special char
+				*(c_write1-3) = 'r';
+				*(c_write2-3) = 'r';			
+				
+			}
+			
+			// set write pointers for next write
 			c_write1 = c_write1 + 2;
 			c_write2 = c_write2 + 2;
-			
-			if (run_len > 1){
-				*(c_write1++) = '0' + run_len;
-				c_write2 = c_write2 + 1;
-			}
 				
 			// start next run
 			run_len = 0; 
