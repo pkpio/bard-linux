316e1f68de8076057735eb861191b787f8ac2195
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-06 06:20:41 +0530

    Updated to correct bulk address in urbs

--- udlfb.c
+++ udlfb.c
@@ -2304,7 +2304,7 @@ static int dlfb_alloc_urb_list(struct dlfb_data *dev, int count, size_t size)
 		// -TODO- Remove hardcoded bulkout address
 		/* urb->transfer_buffer_length set to actual before submit */
 		/*  */
-		usb_fill_bulk_urb(urb, dev->udev, usb_sndbulkpipe(dev->udev, 0x01),
+		usb_fill_bulk_urb(urb, dev->udev, usb_sndbulkpipe(dev->udev, 0x04),
 			buf, size, dlfb_urb_completion, unode);
 		urb->transfer_flags |= URB_NO_TRANSFER_DMA_MAP;
 
